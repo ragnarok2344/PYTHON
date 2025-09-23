d6042360ae5bc77e7b88042d5bdfed3d70df1f73
Pedro <felipe.lemes.leal@escola.pr.gov.br>
2025-09-23 11:53:01 -0300

    exercio 3

---
+++ capitulo5/exercicio 3.py
@@ -0,0 +1,14 @@
+def apresentaacao(nome, nmemae, nomepai):
+    print(f"nome: {nome}")
+    print(f"nomemae: {nomemae}")
+    print(f)
+
+
+
+
+
+
+
+nome = str(input("digite seu nome"))
+nomemae = str(input("digite o nome completo da mae"))
+nomepai = str(input("digite o nome do pai"))
